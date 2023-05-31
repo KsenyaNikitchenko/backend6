b174f745689f197ef4f5ae9689410cfbae3c3807
Ksenya <ksenya.nikitchenko.03@mail.ru>
2023-05-31 10:13:15 +0300

    f

--- admin.php
+++ admin.php
@@ -62,42 +62,54 @@ function show_tables($db){
 }
 function form($db){
   ?>
-  <label for='name'>Имя</label>
-    <input name='name'><br>
-  <label for='email'>E-mail</label>
-    <input name='email'><br>
-  <label for='year'>Год</label>
-  <select name='year'>
-		<?php 
-      echo '<option>' . '' . '</option>';
-			for ($i = 1922; $i <= 2022; $i++)
-				echo '<option>' . $i . '</option>';
-		?>
-	</select><br>
-  <label>Пол:</label>
-	  <input type="radio" value="male" name='gender'>Мужской
-	  <input type="radio" value="female" name=gender>Женский<br>
-  
-  <label>Количество конечностей:</label>
-	  <input type="radio" name='limbs' value='1'>1
-	  <input type="radio" name='limbs' value='2'>2
-	  <input type="radio" name='limbs' value='3'>3
-	  <input type="radio" name='limbs' value='4'>4<br>
-	
-  <label>Сверхспособности:</label>
-	<!-- выводим способности прямо из бд-->
-    <select name="super[]" multiple="multiple">
-	  <?php
-	  	$sql = 'SELECT * FROM superpower';
-	  	foreach ($db->query($sql) as $row) {
-	  		?><option value=<?php print $row['id']?> name=super[]> 
-	  		<?php print $row['name'] . "\t";
-	  	}
-	  ?><br>
-      </select>
-  <label for="biography">Биография</label>
-	  <textarea name='bio'></textarea><br>
-
+  <label>
+                Введите имя:<br>
+                <input name="name" placeholder="Введите имя" /><br>
+            </label>
+            <label>
+                Адрес электронной почты:<br>
+                <input name="email" type="email" placeholder="Введите email" /><br>
+            </label>
+            <label for="year">Год рождения</label>
+            <select name="year">
+    <?php 
+    for ($i = 1922; $i <= 2022; $i++) {
+      printf('<option value="%d">%d</option>', $i, $i);
+    }
+    ?>
+  </select>
+            <br>
+            Выберите пол:<br>
+            <label><input type="radio" checked="checked" name="gender" value="female" />
+                Женский</label>
+            <label><input type="radio" name="gender" value="male" />
+                Мужской</label>
+            <br>
+            Количество конечностей:<br>
+            <label><input type="radio" checked="checked" name="limbs" value="1" />
+                1</label>
+            <label><input type="radio" name="limbs" value="2" />
+                2</label>
+            <label><input type="radio" name="limbs" value="3" />
+                3</label>
+            <label><input type="radio" name="limbs" value="4" />
+                4</label>
+            <label>
+                <br>
+                Сверхспособности:<br>
+                <select name="superpowers[]" multiple="multiple">
+                    <option value="deathless">Бессмертие</option>
+                    <option value="walls" selected="selected">Прохождение сквозь стены</option>
+                    <option value="levitation">Левитация</option>
+                    <option value="elements">Управление стихиями</option>
+                    <option value="time travel">Путешествие во времени</option>
+                </select>
+                <br>
+                Биография:<br>
+                <textarea name="biography">Напишите о себе</textarea><br>
+                <label><input type="checkbox" checked="checked" name="check-kontrol" />
+                    с контрактом ознакомлен(а)</label>
+                <br>
   <?php
 }
 function errors(){
